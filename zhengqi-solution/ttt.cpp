d32cdbfa79258ebf905dbd35f116842bf07b1719
Zhengqi Xi <zhengqixi@gmail.com>
2018-05-22 19:27:39 -0400

    Properly format files

--- zhengqi-solution/ttt.cpp
+++ zhengqi-solution/ttt.cpp
@@ -4,90 +4,84 @@
 
 namespace solution {
 
-  TTT::TTT (const int rows, const int columns) {
-    std::vector < char >toCopy;
-    for (int i = 0; i < columns; ++i) {
-      toCopy.push_back (d_empty);
-    } for (int i = 0; i < rows; ++i) {
-      d_board.push_back (toCopy);
-    }
-  }
-
-  bool TTT::isWinner () const {
-    for (int i = 0; i < d_board.size (); ++i) {
-      if (checkHorizontal (i)) {
-	return true;
-      }
-    }
-    for (int i = 0; i < d_board[0].size (); ++i) {
-      if (checkVertical (i)) {
-	return true;
-      }
-    }
-    return checkDiagonal();
-  }
-
-  bool TTT::checkVertical (const int column) const {
-    for (int i = 0; i < d_board.size (); ++i) {
-      if (d_board[i][column] != d_currentPlayer) {
-	return false;
-      }
-    }
-    return true;
-  }
-
-  bool TTT::checkHorizontal (const int row) const {
-    for (int i = 0; i < d_board[row].size (); ++i) {
-      if (d_board[row][i] != d_currentPlayer) {
-	return false;
-      }
-    }
-    return true;
-  }
-
-  bool TTT::checkDiagonal() const {
-    if (d_board.size() != d_board[0].size()) {
-        return false;
-    }
-    bool isWinner = true;
-    int horizontalIndex = 0;
-    int verticalIndex = 0;
-    while(horizontalIndex != d_board.size() && verticalIndex != d_board.size()) {
-        if (d_board[horizontalIndex][verticalIndex] != d_currentPlayer) {
-            isWinner = false;
-            break;
+    TTT::TTT (const int rows, const int columns) {
+        std::vector < char >toCopy;
+        for(int i = 0; i < columns; ++i) {
+            toCopy.push_back (d_empty);
+        } for(int i = 0; i < rows; ++i) {
+            d_board.push_back (toCopy);
         }
-        ++horizontalIndex;
-        ++verticalIndex;
     }
-    if (isWinner) {
+
+    bool TTT::isWinner () const {
+        for(int i = 0; i < d_board.size (); ++i) {
+            if(checkHorizontal (i)) {
+                return true;
+        }} for(int i = 0; i < d_board[0].size (); ++i) {
+            if(checkVertical (i)) {
+                return true;
+            }
+        }
+        return checkDiagonal ();
+    }
+
+    bool TTT::checkVertical (const int column)const {
+        for(int i = 0; i < d_board.size (); ++i) {
+            if(d_board[i][column] != d_currentPlayer) {
+                return false;
+        }} return true;
+    }
+
+    bool TTT::checkHorizontal (const int row)const {
+        for(int i = 0; i < d_board[row].size (); ++i) {
+            if(d_board[row][i] != d_currentPlayer) {
+                return false;
+        }} return true;
+    }
+
+    bool TTT::checkDiagonal () const {
+        if(d_board.size () != d_board[0].size ()) {
+            return false;
+        } bool isWinner = true;
+        int horizontalIndex = 0;
+        int verticalIndex = 0;
+        while(horizontalIndex != d_board.size ()
+              && verticalIndex != d_board.size ()) {
+            if(d_board[horizontalIndex][verticalIndex] != d_currentPlayer) {
+                isWinner = false;
+                break;
+            }
+            ++horizontalIndex;
+            ++verticalIndex;
+        }
+        if(isWinner) {
+            return isWinner;
+        }
+        isWinner = true;
+        horizontalIndex = d_board.size () - 1;
+        verticalIndex = 0;
+        while(horizontalIndex != -1 && verticalIndex != d_board.size ()) {
+            if(d_board[horizontalIndex][verticalIndex] != d_currentPlayer) {
+                isWinner = false;
+                break;
+            }
+            --horizontalIndex;
+            ++verticalIndex;
+        }
         return isWinner;
     }
-    isWinner = true;
-    horizontalIndex = d_board.size() - 1;
-    verticalIndex = 0;
-    while(horizontalIndex != -1 && verticalIndex != d_board.size()) {
-        if (d_board[horizontalIndex][verticalIndex] != d_currentPlayer) {
-            isWinner = false;
-            break;
-        }
-        --horizontalIndex;
-        ++verticalIndex;
-    }
-    return isWinner;
-  }
-  
 
-  std::ostream & operator<< (std::ostream & os, const TTT& board) {
-    for (auto it = board.d_board.begin (); it != board.d_board.end (); ++it) {
-      os << '|';
-      for (auto jt = it->begin (); jt != it->end (); ++jt) {
-	os << *jt;
-	os << '|';
-      }
-      os << '\n';
+    std::ostream & operator<< (std::ostream & os, const TTT & board) {
+        for(auto it = board.d_board.begin (); it != board.d_board.end ();
+            ++it) {
+            os << '|';
+            for(auto jt = it->begin (); jt != it->end (); ++jt) {
+                os << *jt;
+                os << '|';
+            }
+            os << '\n';
+        }
+        return os;
     }
-    return os;
-  }
 
 }
--- zhengqi-solution/ttt.m.cpp
+++ zhengqi-solution/ttt.m.cpp
@@ -2,45 +2,48 @@
 #include "ttt.h"
 
 void
-getInput (solution::TTT& board) {
+getInput (solution::TTT & board) {
     int row = 0;
     int column = 0;
-    while (1) {
+    while(1) {
         std::cout << "Row: ";
         std::cin >> row;
         std::cout << "Column: ";
         std::cin >> column;
-        if (board.insert(row, column)) {
+        if(board.insert (row, column)) {
             return;
         }
-        std::cout << "Space is already taken/Invalid location. Please reselect!\n";
+        std::
+            cout <<
+            "Space is already taken/Invalid location. Please reselect!\n";
     }
 }
 
-bool turn(const int playerNum, solution::TTT& board) {
-    std::cout << "Player " << playerNum << "'s turn:\n"; 
+bool
+turn (const int playerNum, solution::TTT & board) {
+    std::cout << "Player " << playerNum << "'s turn:\n";
     std::cout << board;
-    getInput(board);
-      if (board.isWinner()) {
-      std::cout << "Player " << playerNum << " is winner\n";
-      std::cout << board;
-      return true;
+    getInput (board);
+    if(board.isWinner ()) {
+        std::cout << "Player " << playerNum << " is winner\n";
+        std::cout << board;
+        return true;
     }
-    board.switchPlayer();
+    board.switchPlayer ();
     return false;
 }
 
 int
 main () {
-  solution::TTT game;
-  std::cout << "Game start\n";
-  while (1) {
-    if (turn(1, game)) {
-        break;
+    solution::TTT game;
+    std::cout << "Game start\n";
+    while(1) {
+        if(turn (1, game)) {
+            break;
+        }
+        if(turn (2, game)) {
+            break;
+        }
     }
-    if (turn(2, game)) {
-        break;
-    }
-  }
-  return 0;
+    return 0;
 }
